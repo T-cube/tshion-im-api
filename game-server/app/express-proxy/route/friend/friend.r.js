fe5a61e9109cc15a1e87984b8f429add74c8755f
xuezier <xzj15859722542@hotmail.com>
2018-08-03 17:04:56 +0800

    fix friend list showname

--- game-server/app/express-proxy/route/friend/friend.r.js
+++ game-server/app/express-proxy/route/friend/friend.r.js
@@ -13,20 +13,16 @@ module.exports = function(app) {
         method(req, res, next) {
           var user = req.user;
 
-          User.getAllFriendsInfo(user._id).then(friends => {
-            res.sendJson(friends);
+          User.getAllFriendsInfo(user._id).then(list => {
+            return Room.getUserRoomMap(user._id.toHexString()).then(rooms => {
+              var result = rooms.map(room => {
+                var friend = list.find(item => ~room.members.indexOf(item._id.toString()));
+                friend.roomid = room.roomid;
+                return friend;
+              });
+              res.sendJson(result);
+            });
           }).catch(next);
-
-          // User.getAllFriendsInfo(user._id).then(list => {
-          //   return Room.getUserRoomMap(user._id.toHexString()).then(rooms => {
-          //     var result = rooms.map(room => {
-          //       var friend = list.find(item => ~room.members.indexOf(item._id.toString()));
-          //       friend.roomid = room.roomid;
-          //       return friend;
-          //     });
-          //     res.sendJson(result);
-          //   });
-          // }).catch(next);
         }
       }
     }
